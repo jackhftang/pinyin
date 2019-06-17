a510344897a926e732c844ebdf26c586bb0b5cb8
Jack Tang <me@jackhftang.com>
2019-06-18 00:13:33 +0800

    ADD: added pinyin selection

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:flutter/services.dart' show rootBundle;
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 
-Map<String, dynamic> dictionary;
+Map<String, List<String>> dictionary = {};
 
 void main() async {
   var str = await rootBundle.loadString('assets/pinyin.json');
-  dictionary = jsonDecode(str);
+  jsonDecode(str).forEach((k, v) {
+    dictionary[k] = List.from(v);
+  });
   runApp(MyApp());
 }
 
@@ -64,22 +66,23 @@ class _MyHomePageState extends State<MyHomePage> {
       String text = await file.readAsString();
       var lis = jsonDecode(text);
 
+      // rebuild history
       history = [];
       for (var line in lis) {
-        var phrase = [];
+        List<Word> phrase = [];
         for (var word in line) {
           phrase.add(Word(word['text'], dictionary[word['text']]));
         }
         history.add(phrase);
       }
     } catch (e) {
-      print("Couldn't read file");
+      print("Couldn't read file $e");
     }
   }
 
   _save() async {
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/history.json');
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File('${dir.path}/history.json');
     final text = jsonEncode(history);
     print('saving');
     await file.writeAsString(text);
@@ -117,59 +120,109 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  delete(int i) {
+    setState(() {
+      history.removeAt(i);
+      _save();
+    });
+  }
+
   Widget buildWords(BuildContext context, List<Word> words) {
     return Row(
       children: words.map((word) {
         return Container(
           padding: EdgeInsets.all(5.0),
-          child: Column(
-            children: <Widget>[
-              Text(word.pinyin, textAlign: TextAlign.left),
-              Text(word.text, textAlign: TextAlign.left),
-            ],
-          )
-        );
+          child: GestureDetector(
+            child: Column(
+              children: <Widget>[
+                Text(word.pinyin, textAlign: TextAlign.left),
+                Text(word.text, textAlign: TextAlign.left),
+              ],
+            ),
+            onTap: () async {
+              if (word.pinyins.length == 1) {
+                final snackBar = SnackBar(
+                  content: Text('${word.text} 并無其他拼音'),
+                  duration: Duration(seconds: 1)
+                );
+                Scaffold.of(context).showSnackBar(snackBar);
+              } else {
+                int i = await showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return SimpleDialog(
+                      title: Text('選擇拼音', textAlign: TextAlign.center),
+                      children: List.generate(word.pinyins.length, (int i) {
+                        var w = word.pinyins[i];
+                        return SimpleDialogOption(
+                          child: Text(w),
+                          onPressed: () {
+                            Navigator.pop(context, i);
+                          },
+                        );
+                      })
+                    );
+                  }
+                );
+                setState(() {
+                  word.index = i;
+                  _save();
+                });
+              }
+            }
+          ));
       }).toList()
     );
   }
 
+  Widget buildHistory(context) {
+    List<Widget> children = [];
+    for (var i = history.length - 1; i >= 0; i--) {
+      var words = history[i];
+      children.add(Row(
+        children: [
+          Expanded(child: buildWords(context, words)),
+          FlatButton(child: Text('X'), onPressed: () => delete(i)),
+        ]
+      ));
+    }
+    return Column(children: children);
+  }
+
   @override
   Widget build(BuildContext context) {
+    for (var i = 0; i < history.length; i++) {
+
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Row(
-                children: [
-                  Expanded(child: TextField(controller: controller)),
-                  FlatButton(child: Text('save'),
-                    color: Colors.blue,
-                    textColor: Colors.white,
-                    onPressed: save,
-                  )
-                ]
-              )
-            ),
-            buildWords(context, words),
-            Divider(),
-            Column(
-              children: history.reversed.map((words) {
-                return Row(
+      body: Builder(builder: (context) =>
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Row(
                   children: [
-                    Expanded(child: buildWords(context, words)),
-                    FlatButton(child: Text('X')),
+                    Expanded(child: TextField(controller: controller)),
+                    FlatButton(
+                      child: Text('save'),
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      onPressed: save,
+                    )
                   ]
-                );
-              }).toList(),
-            )
-          ],
-        ),
+                )
+              ),
+              buildWords(context, words),
+              Divider(),
+              buildHistory(context),
+            ],
+          ),
+        )
       ),
     );
   }
