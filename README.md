e2225b17e45ee48061201d7647cceec145e7858c
Jack Tang <me@jackhftang.com>
2019-07-26 20:21:05 +0800

    UPT: remove the debug badge

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 If you know how to type traditional chinese and learning putonghua pinyin,
 this is a a Ad-free android apk that you can lookup and save your own dictionary.
 
-
-
- 
--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: '拼音字典',
       theme: ThemeData(
         primarySwatch: Colors.blue,
